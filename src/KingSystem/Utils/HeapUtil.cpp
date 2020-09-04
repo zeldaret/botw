977ff07c85a0dd4db08f4e8b60a991b715f5a2af
Léo Lam <leo@leolam.fr>
2020-09-04 22:38:06 +0200

    ksys: Implement more heap utilities

--- src/KingSystem/Utils/HeapUtil.cpp
+++ src/KingSystem/Utils/HeapUtil.cpp
@@ -4,6 +4,26 @@
 
 namespace ksys::util {
 
+// Name, layout and purpose unknown; this is stubbed in release builds
+class DebugHeapHolder {
+    SEAD_SINGLETON_DISPOSER(DebugHeapHolder)
+public:
+    virtual ~DebugHeapHolder() = default;
+    virtual sead::Heap* getHeap() { return nullptr; }
+};
+
+SEAD_SINGLETON_DISPOSER_IMPL(DebugHeapHolder)
+
+// Name, layout and purpose unknown; this is stubbed in release builds
+class DebugHeapHolder2 {
+    SEAD_SINGLETON_DISPOSER(DebugHeapHolder2)
+public:
+    virtual ~DebugHeapHolder2() = default;
+    virtual sead::Heap* getHeap() { return nullptr; }
+};
+
+SEAD_SINGLETON_DISPOSER_IMPL(DebugHeapHolder2)
+
 sead::Heap* getHeapOrCurrentHeap(sead::Heap* heap) {
     if (heap)
         return heap;
@@ -11,7 +31,29 @@ sead::Heap* getHeapOrCurrentHeap(sead::Heap* heap) {
     if (!sead::ThreadMgr::instance())
         return nullptr;
 
+    return getCurrentHeap();
+}
+
+void adjustHeapOrCurrentHeap(sead::Heap* heap) {
+    if (!heap)
+        heap = getCurrentHeap();
+    heap->adjust();
+}
+
+sead::Heap* getCurrentHeap() {
     return sead::HeapMgr::instance()->getCurrentHeap();
+}
+
+sead::Heap* getDebugHeap() {
+    if (!DebugHeapHolder::instance())
+        return nullptr;
+    return DebugHeapHolder::instance()->getHeap();
+}
+
+sead::Heap* getDebugHeap2() {
+    if (DebugHeapHolder2::instance())
+        return DebugHeapHolder2::instance()->getHeap();
+    return getDebugHeap();
 }
 
 }  // namespace ksys::util
--- src/KingSystem/Utils/HeapUtil.h
+++ src/KingSystem/Utils/HeapUtil.h
@@ -11,4 +11,10 @@ namespace ksys::util {
 /// Returns the specified heap if it is not null. Otherwise, this returns the current heap.
 sead::Heap* getHeapOrCurrentHeap(sead::Heap* heap);
 
+void adjustHeapOrCurrentHeap(sead::Heap* heap);
+
+sead::Heap* getCurrentHeap();
+sead::Heap* getDebugHeap();
+sead::Heap* getDebugHeap2();
+
 }  // namespace ksys::util
