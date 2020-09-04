578955f4e9b0d352a6a9c70288d8e988a1ec2717
Léo Lam <leo@leolam.fr>
2020-09-04 20:07:49 +0200

    ksys/res: Implement Resource (base class)

---
+++ src/KingSystem/Resource/resResource.cpp
@@ -0,0 +1,38 @@
+#include "KingSystem/Resource/resResource.h"
+
+namespace ksys::res {
+
+Resource::Resource() = default;
+
+Context* Resource::getContext() const {
+    return mContext;
+}
+
+void Resource::setContext(Context* context) {
+    mContext = context;
+}
+
+void Resource::onDestroy() {
+    onDestroy_();
+}
+
+void Resource::parse(Context*, sead::Heap* heap) {
+    parse_(mRawData, mRawSize, heap);
+}
+
+bool Resource::m5() {
+    m5_();
+    return true;
+}
+
+bool Resource::finishParsing() {
+    const bool ret = finishParsing_();
+    mContext = nullptr;
+    return ret;
+}
+
+bool Resource::m7() {
+    return m7_();
+}
+
+}  // namespace ksys::res
---
+++ src/KingSystem/Resource/resResource.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <resource/seadResource.h>
+#include "KingSystem/Utils/Types.h"
+
+namespace ksys::res {
+
+class IResource {
+public:
+    virtual ~IResource() = default;
+};
+
+class Context {
+public:
+    virtual ~Context() = default;
+};
+
+class Resource : public sead::DirectResource, public IResource {
+    SEAD_RTTI_OVERRIDE(Resource, sead::DirectResource)
+public:
+    Resource();
+
+    void doCreate_(u8* buffer, u32 bufferSize, sead::Heap* heap) override;
+
+    virtual bool needsParse() const { return false; }
+    virtual bool m2_() { return true; }
+
+    Context* getContext() const;
+    void setContext(Context* context);
+
+    void onDestroy();
+    void parse(Context*, sead::Heap* heap);
+    bool m5();
+    bool finishParsing();
+    bool m7();
+
+protected:
+    virtual void onDestroy_() {}
+    virtual bool parse_(u8* data, size_t size, sead::Heap* heap);
+    virtual void m5_() {}
+    virtual bool finishParsing_() { return true; }
+    virtual bool m7_() { return true; }
+    virtual void m8_() {}
+
+    u32 mAllocSize = 0;
+    Context* mContext = nullptr;
+};
+KSYS_CHECK_SIZE_NX150(Resource, 0x38);
+
+inline void Resource::doCreate_(u8*, u32, sead::Heap*) {}
+
+inline bool Resource::parse_(u8*, size_t, sead::Heap*) {
+    return true;
+}
+
+}  // namespace ksys::res
