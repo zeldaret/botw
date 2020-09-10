6af5dfd74748f13d8c375c49d0c3383fd4050c35
Léo Lam <leo@leolam.fr>
2020-09-10 12:35:27 +0200

    ksys/Utils: Add Event

---
+++ src/KingSystem/Utils/Thread/Event.cpp
@@ -0,0 +1,50 @@
+#include "KingSystem/Utils/Thread/Event.h"
+
+namespace ksys::util {
+
+Event::Event() = default;
+
+Event::Event(sead::Heap* disposer_heap) : mEvent(disposer_heap) {}
+
+Event::Event(bool manual_reset) : mManualReset(manual_reset), mEvent(manual_reset) {}
+
+Event::Event(sead::Heap* disposer_heap, bool manual_reset)
+    : mManualReset(manual_reset), mEvent(disposer_heap, manual_reset) {}
+
+Event::Event(sead::Heap* disposer_heap, sead::IDisposer::HeapNullOption heap_null_option,
+             bool manual_reset)
+    : mManualReset(manual_reset), mEvent(disposer_heap, heap_null_option, manual_reset) {}
+
+Event::~Event() = default;
+
+void Event::initialize(bool manual_reset) {
+    mManualReset = manual_reset;
+    mEvent.initialize(manual_reset);
+}
+
+void Event::wait() {
+    if (!mSignalSet || !mManualReset)
+        mEvent.wait();
+}
+
+bool Event::wait(sead::TickSpan duration) {
+    return (mSignalSet && mManualReset) || mEvent.wait(duration);
+}
+
+void Event::setSignal() {
+    if (mSignalSet == 1 && mManualReset)
+        return;
+
+    mSignalSet.exchange(1);
+    mEvent.setSignal();
+}
+
+void Event::resetSignal() {
+    if (!mSignalSet && mManualReset)
+        return;
+
+    mEvent.resetSignal();
+    mSignalSet.exchange(0);
+}
+
+}  // namespace ksys::util
---
+++ src/KingSystem/Utils/Thread/Event.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <thread/seadEvent.h>
+#include "KingSystem/Utils/Types.h"
+#include <thread/seadAtomic.h>
+
+namespace ksys::util {
+
+class Event {
+public:
+    Event();
+    explicit Event(sead::Heap* disposer_heap);
+    explicit Event(bool manual_reset);
+    Event(sead::Heap* disposer_heap, bool manual_reset);
+    Event(sead::Heap* disposer_heap, sead::IDisposer::HeapNullOption heap_null_option,
+          bool manual_reset);
+    virtual ~Event();
+
+    void initialize(bool manual_reset);
+    void wait();
+    bool wait(sead::TickSpan duration);
+    void setSignal();
+    void resetSignal();
+
+private:
+    bool mManualReset = false;
+    sead::Atomic<u32> mSignalSet = 0;
+    sead::Event mEvent;
+};
+KSYS_CHECK_SIZE_NX150(sead::Event, 0x30);
+KSYS_CHECK_SIZE_NX150(Event, 0x40);
+
+}  // namespace ksys::util
