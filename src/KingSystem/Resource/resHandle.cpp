22a0c93c514745e3499d186693e3198cf3598fc6
Léo Lam <leo@leolam.fr>
2020-09-22 19:36:59 +0200

    ksys/res: Add status descriptions

--- src/KingSystem/Resource/resHandle.cpp
+++ src/KingSystem/Resource/resHandle.cpp
@@ -1,6 +1,42 @@
 #include "KingSystem/Resource/resHandle.h"
+#include <container/seadBuffer.h>
+#include <prim/seadSafeString.h>
+#include <time/seadTickTime.h>
 
 namespace ksys::res {
+
+namespace {
+struct UnusedVariables {
+    sead::TickTime tick_time;
+    void* _8 = nullptr;
+    f32 _10 = 0.5;
+    f32 _14 = 1.0;
+    f32 _18 = 0.5;
+    f32 _1c = 1.0;
+};
+}  // namespace
+
+UnusedVariables sVariables;
+const sead::SafeString sStatusStrings_[0x11] = {
+    "            ",    // 0x0
+    "[Unknown]   ",    // 0x1
+    "[No File]   ",    // 0x2
+    "[Request OK]",    // 0x3
+    "[Already]   ",    // 0x4
+    "[FromCache] ",    // 0x5
+    "[No Memory] ",    // 0x6
+    "[No Execute]",    // 0x7
+    "[Size Zero] ",    // 0x8
+    "[Compose NG]",    // 0x9
+    "[AllocSizeNG]",   // 0xa
+    "[Edited]    ",    // 0xb
+    "[HeapSizeNG]",    // 0xc
+    "[FileDeviceNG]",  // 0xd
+    "[Load OK]",       // 0xe
+    "[Yet]",           // 0xf
+    "[Canceled]",      // 0x10
+};
+const sead::Buffer<const sead::SafeString> sStatusStrings{0x11, sStatusStrings_};
 
 Handle::Handle() = default;
 
