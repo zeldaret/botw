f602b5c6be9be70df3cb8e1f97767b52ccc65ada
Léo Lam <leo@leolam.fr>
2020-11-04 00:40:44 +0100

    ksys/res: Declare registerEntryFactory
    Implementation coming soon.

--- src/KingSystem/Resource/resSystem.h
+++ src/KingSystem/Resource/resSystem.h
@@ -4,6 +4,10 @@
 
 namespace ksys::res {
 
+class EntryFactoryBase;
+
+void registerEntryFactory(EntryFactoryBase* factory, const sead::SafeString& name);
+
 // In release builds, the only thing this function does is return 1.
 // TODO: figure out what this is used for. Stubbed log function?
 bool stubbedLogFunction();
