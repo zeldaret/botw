0b6cdede6d4e867d2ad59ca007ee987f3ef6c300
Léo Lam <leo@leolam.fr>
2020-11-04 12:08:53 +0100

    Add functions to CSV and fix matching issue in InitTimeInfoEx

--- src/KingSystem/Utils/InitTimeInfo.h
+++ src/KingSystem/Utils/InitTimeInfo.h
@@ -23,7 +23,10 @@ KSYS_CHECK_SIZE_NX150(InitTimeInfo, 0x10);
 
 class InitTimeInfoEx {
 public:
-    explicit InitTimeInfoEx(sead::TickTime start_time = {}) : mInfo(start_time) {}
+    explicit InitTimeInfoEx(sead::TickTime start_time = {}) : mInfo(start_time) {
+        _0 = 0;
+        _4 = 0x8004EF;
+    }
 
     u32 getField0() const { return _0; }
     u32 getField4() const { return _4; }
@@ -33,7 +36,7 @@ public:
 
 private:
     u32 _0 = 0;
-    u32 _4 = 0x8004EF;
+    u32 _4 = 0;
     InitTimeInfo mInfo;
 };
 KSYS_CHECK_SIZE_NX150(InitTimeInfoEx, 0x18);
