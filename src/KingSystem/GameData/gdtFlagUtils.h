bfa4a2553e188dafba168dd9815b8b773f6a1d96
Léo Lam <leo@leolam.fr>
2020-11-07 19:06:12 +0100

    ksys/gdt: Add getBool helper

--- src/KingSystem/GameData/gdtFlagUtils.h
+++ src/KingSystem/GameData/gdtFlagUtils.h
@@ -2,4 +2,16 @@
 
 #include "KingSystem/GameData/gdtManager.h"
 
-namespace ksys::gdt {}  // namespace ksys::gdt
+namespace ksys::gdt {
+
+inline bool getBool(FlagHandle handle, bool debug = false) {
+    bool value{};
+
+    auto* mgr = Manager::instance();
+    if (mgr)
+        mgr->getBool(handle, &value, debug);
+
+    return value;
+}
+
+}  // namespace ksys::gdt
