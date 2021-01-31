eaa0f651efa89bd6bff75fcbb482fc57a5b7d77a
Léo Lam <leo@leolam.fr>
2021-01-31 22:30:15 +0100

    tools/diff: Rebuild on library changes as well

--- tools/diff_settings.py
+++ tools/diff_settings.py
@@ -5,7 +5,7 @@ def apply(config, args):
     config['arch'] = 'aarch64'
     config['baseimg'] = 'data/main.elf'
     config['myimg'] = 'build/uking'
-    config['source_directories'] = ['src']
+    config['source_directories'] = ['src', 'lib']
     config['objdump_executable'] = 'tools/aarch64-none-elf-objdump'
 
     for dir in ('build', 'build/nx64-release'):
