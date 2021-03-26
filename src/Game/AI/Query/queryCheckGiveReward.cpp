1bd2f03de8a21803fb192a09f37fd52b43b44cd1
Léo Lam <leo@leolam.fr>
2021-03-26 19:10:28 +0100

    query: Implement CheckGiveReward

--- src/Game/AI/Query/queryCheckGiveReward.cpp
+++ src/Game/AI/Query/queryCheckGiveReward.cpp
@@ -7,13 +7,8 @@ CheckGiveReward::CheckGiveReward(const InitArg& arg) : ksys::act::ai::Query(arg)
 
 CheckGiveReward::~CheckGiveReward() = default;
 
-// FIXME: implement
 int CheckGiveReward::doQuery() {
-    return -1;
+    return 0;
 }
-
-void CheckGiveReward::loadParams(const evfl::QueryArg& arg) {}
-
-void CheckGiveReward::loadParams() {}
 
 }  // namespace uking::query
--- src/Game/AI/Query/queryCheckGiveReward.h
+++ src/Game/AI/Query/queryCheckGiveReward.h
@@ -10,9 +10,6 @@ public:
     explicit CheckGiveReward(const InitArg& arg);
     ~CheckGiveReward() override;
     int doQuery() override;
-
-    void loadParams() override;
-    void loadParams(const evfl::QueryArg& arg) override;
 };
 
 }  // namespace uking::query
