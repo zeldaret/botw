3fc1957cc24f1d92b26d17a633654ed3a75d9de2
Léo Lam <leo@leolam.fr>
2021-04-14 19:59:01 +0200

    ksys/res: Implement RagdollConfig

--- src/KingSystem/Resource/resResourceRagdollConfig.cpp
+++ src/KingSystem/Resource/resResourceRagdollConfig.cpp
@@ -1,1 +1,22 @@
 #include "KingSystem/Resource/resResourceRagdollConfig.h"
+
+namespace ksys::res {
+
+RagdollConfig::RagdollConfig() : ParamIO("rgconfig", 0) {}
+
+RagdollConfig::~RagdollConfig() = default;
+
+void RagdollConfig::doCreate_(u8* buffer, u32 buffer_size, sead::Heap* heap) {}
+
+bool RagdollConfig::parse_(u8* data, size_t size, sead::Heap* heap) {
+    if (!data)
+        return false;
+
+    addList(&mConfig, "ConfigRoot");
+
+    agl::utl::ResParameterArchive archive{data};
+    applyResParameterArchive(archive);
+    return true;
+}
+
+}  // namespace ksys::res
