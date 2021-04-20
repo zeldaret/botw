63472349c75cfe9ed8d2424b40bd935683bdba31
Léo Lam <leo@leolam.fr>
2021-04-20 18:53:19 +0200

    ksys/res: Implement AISchedule

--- src/KingSystem/Resource/resResourceAISchedule.cpp
+++ src/KingSystem/Resource/resResourceAISchedule.cpp
@@ -1,1 +1,9 @@
 #include "KingSystem/Resource/resResourceAISchedule.h"
+
+namespace ksys::res {
+
+void AISchedule::doCreate_(u8* buffer, u32 buffer_size, sead::Heap* heap) {
+    mData = buffer;
+}
+
+}  // namespace ksys::res
