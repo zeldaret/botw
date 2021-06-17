328354f85741b160ccaa9f9e219666dd842e37fa
Léo Lam <leo@leolam.fr>
2021-06-17 23:51:57 +0200

    ksys/evt: Implement eventFlowAlloc/Free

--- src/KingSystem/Event/evtEventResource.cpp
+++ src/KingSystem/Event/evtEventResource.cpp
@@ -1,1 +1,19 @@
 #include "KingSystem/Event/evtEventResource.h"
+#include <heap/seadHeap.h>
+
+namespace ksys::evt {
+
+void* eventFlowAlloc(size_t size, size_t alignment, void* userdata) {
+    auto* heap = static_cast<sead::Heap*>(userdata);
+    if (!heap)
+        return nullptr;
+    return heap->tryAlloc(size, static_cast<int>(alignment));
+}
+
+void eventFlowFree(void* ptr, void* userdata) {
+    auto* heap = static_cast<sead::Heap*>(userdata);
+    if (heap)
+        heap->free(ptr);
+}
+
+}  // namespace ksys::evt
