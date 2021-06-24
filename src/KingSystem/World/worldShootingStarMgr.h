0eef5b39333c70f5ce30095e28b1a08e87a273d8
theo3 <arisstephenson2@gmail.com>
2021-06-24 00:22:55 -0700

    shootingStarMgr

--- src/KingSystem/World/worldShootingStarMgr.h
+++ src/KingSystem/World/worldShootingStarMgr.h
@@ -1,20 +1,44 @@
 #pragma once
 
+#include <math/seadVector.h>
+#include <thread/seadAtomic.h>
 #include "KingSystem/Utils/Types.h"
 #include "KingSystem/World/worldJob.h"
 
 namespace ksys::world {
 
-// TODO
 class ShootingStarMgr : public Job {
 public:
     ShootingStarMgr();
+    virtual ~ShootingStarMgr();
 
     JobType getType() const override { return JobType::ShootingStar; }
 
+    virtual void init_(sead::Heap* heap) override;
+    virtual void calc_() override;
     virtual void reset();
 
-    u8 _20[0x30 - 0x20];
+    static void setScheduled(bool enable);
+    void initSchedule();
+    static bool isScheduledTime();
+
+    bool tryGetStarPosition(sead::Vector3f* out) const;
+    static void setStarPosition(f32 x, f32 y, f32 z);
+    static void spawnShootingStar();
+    void spawnStar();
+
+    static bool checkCamera();
+    bool isStarPositionValid();
+    static void setShootingStarHours(int start, int end, int hr_unk);
+    static void resetStarPosition();
+    static void resetStarProperty();
+
+    s32 mFallHour = 0;
+    s32 mFallMinute = 0;
+    bool mInitialised = false;
+    bool mNeedSpawnStar = false;
+    bool mIsMainField = false;
+    bool _2b = false;
 };
 KSYS_CHECK_SIZE_NX150(ShootingStarMgr, 0x30);
 
