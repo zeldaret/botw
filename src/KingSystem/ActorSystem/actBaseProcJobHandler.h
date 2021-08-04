af2207632e4a07cc2c16615807521a69d2dad3c4
Léo Lam <leo@leolam.fr>
2021-08-05 00:44:30 +0200

    ksys/act: Implement BaseProcJobHandlerT

--- src/KingSystem/ActorSystem/actBaseProcJobHandler.h
+++ src/KingSystem/ActorSystem/actBaseProcJobHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <prim/seadDelegate.h>
 #include "KingSystem/ActorSystem/actBaseProcJob.h"
 #include "KingSystem/Utils/Types.h"
 
@@ -19,5 +20,18 @@ protected:
     BaseProcJobLink mLink;
 };
 KSYS_CHECK_SIZE_NX150(BaseProcJobHandler, 0x30);
+
+/// For binding actor member functions.
+template <typename T>
+class BaseProcJobHandlerT : public BaseProcJobHandler {
+public:
+    BaseProcJobHandlerT(BaseProc* proc, void (T::*fn)())
+        : BaseProcJobHandler(proc), mDelegate(proc, fn) {}
+
+    void invoke() override { mDelegate.invoke(); }
+
+private:
+    sead::Delegate<T> mDelegate;
+};
 
 }  // namespace ksys::act
