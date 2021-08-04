d8ca697d3158934598e2d42b297fe63127251a7a
MonsterDruide1 <5958456@gmail.com>
2021-08-05 01:33:20 +0200

    ksys/act: Add LinkTag

--- src/KingSystem/ActorSystem/actActor.h
+++ src/KingSystem/ActorSystem/actActor.h
@@ -5,6 +5,7 @@
 #include <prim/seadTypedBitFlag.h>
 #include "KingSystem/ActorSystem/actBaseProc.h"
 #include "KingSystem/Map/mapMubinIter.h"
+#include "KingSystem/Map/mapObjectLink.h"
 
 namespace ksys {
 
@@ -90,7 +91,7 @@ public:
 
     static constexpr size_t getCreatorListNodeOffset() { return offsetof(Actor, mCreatorListNode); }
 
-protected:
+//protected:
     friend class ActorCreator;
 
     /* 0x17c */ u8 TEMP_0x17c[0x518 - 0x17c];  // FIXME
@@ -105,7 +106,8 @@ protected:
     /* 0x658 */ u8 TEMP_0x650[0x710 - 0x658];
     /* ..... */  // The name could be incorrect.
     /* 0x710 */ sead::TypedBitFlag<StasisFlag> mStasisFlags;
-    /* 0x714 */ u8 TEMP_0x714[0x7b0 - 0x714];  // FIXME
+    /* 0x714 */ map::MapLinkDefType mEmittedSignalType;
+    /* 0x718 */ u8 TEMP_0x714[0x7b0 - 0x718];  // FIXME
     /* 0x7b0 */ ActorCreator* mCreator{};
     /* 0x7b8 */ sead::ListNode mCreatorListNode;
     /* 0x7c8 */ map::Object* mMapObject;
