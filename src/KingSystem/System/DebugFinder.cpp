488f2c3e71af8d443c1fc04d0f03e70e0aa7aefd
iTNTPiston <zhaoyizhu2000@gmail.com>
2021-08-09 16:40:04 -0700

    Implemented DebugFinder

---
+++ src/KingSystem/System/DebugFinder.cpp
@@ -0,0 +1,14 @@
+#include "KingSystem/System/DebugFinder.h"
+
+namespace ksys {
+
+SEAD_SINGLETON_DISPOSER_IMPL(DebugFinder)
+
+DebugFinder::DebugFinder() = default;
+
+void DebugFinder::init(sead::Heap* heap) {
+    
+}
+
+}
+
