b028cb32645e1d18628e86b95e11476dbd9bac8d
Léo Lam <leo@leolam.fr>
2021-12-19 12:11:15 +0100

    Havok: Use hkFloat32 instead of hkReal in hkVector4f
    hkVector4f is for floats

--- lib/hkStubs/Havok/Common/Base/Math/Vector/hkVector4f.h
+++ lib/hkStubs/Havok/Common/Base/Math/Vector/hkVector4f.h
@@ -9,28 +9,28 @@ public:
     HK_DECLARE_CLASS_ALLOCATOR(hkVector4f)
 
     HK_FORCE_INLINE hkVector4f() {}
-    HK_FORCE_INLINE hkVector4f(hkReal x, hkReal y, hkReal z, hkReal w = 0);
+    HK_FORCE_INLINE hkVector4f(hkFloat32 x, hkFloat32 y, hkFloat32 z, hkFloat32 w = 0);
 
     // This prevents hkVector4f from being passed in registers.
     // NOLINTNEXTLINE(modernize-use-equals-default)
     HK_FORCE_INLINE hkVector4f(const hkVector4f& other) : v(other.v) {}
 
-    HK_FORCE_INLINE void set(hkReal x, hkReal y, hkReal z, hkReal w = 0);
-    HK_FORCE_INLINE void setAll(hkReal x);
+    HK_FORCE_INLINE void set(hkFloat32 x, hkFloat32 y, hkFloat32 z, hkFloat32 w = 0);
+    HK_FORCE_INLINE void setAll(hkFloat32 x);
 
     void sub_7100FABE80(const hkVector4f&, const hkVector4f&);
 
     m128 v;
 };
 
-inline hkVector4f::hkVector4f(hkReal x, hkReal y, hkReal z, hkReal w) {
+inline hkVector4f::hkVector4f(hkFloat32 x, hkFloat32 y, hkFloat32 z, hkFloat32 w) {
     v[0] = x;
     v[1] = y;
     v[2] = z;
     v[3] = w;
 }
 
-inline void hkVector4f::set(hkReal x, hkReal y, hkReal z, hkReal w) {
+inline void hkVector4f::set(hkFloat32 x, hkFloat32 y, hkFloat32 z, hkFloat32 w) {
     v[0] = x;
     v[1] = y;
     v[2] = z;
