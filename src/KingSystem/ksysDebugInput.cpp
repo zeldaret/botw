24b82f6f53f0b903d5efd4674a481734d4a31856
Brian Savage <savage13@gmail.com>
2021-12-30 17:02:53 -0500

    Fix formatting changes

--- src/KingSystem/ksysDebugInput.cpp
+++ src/KingSystem/ksysDebugInput.cpp
@@ -1,15 +1,15 @@
 
-#include "KingSystem/System/VFR.h"
 #include "KingSystem/ksysDebugInput.h"
+#include "KingSystem/System/VFR.h"
 
 namespace ksys {
 
 SEAD_SINGLETON_DISPOSER_IMPL(DebugInput)
 
 void DebugInput::update() {
-    if(mFlags.isOnBit(0)) {
+    if (mFlags.isOnBit(0)) {
         mLastDelta += VFR::instance()->getDeltaTime();
-        while(mLastDelta > 30.0) {
+        while (mLastDelta > 30.0) {
             mLastDelta -= 30.0;
         }
     }
