29a7b8f3493f61a7cdc50c64e1d12952fc1598a3
Léo Lam <leo@leolam.fr>
2022-01-25 22:32:59 +0100

    Add Doxygen and m.css config file

---
+++ docs/doxygen_config.py
@@ -0,0 +1,8 @@
+# Configuration file for https://mcss.mosra.cz/documentation/doxygen/
+DOXYFILE = "Doxyfile"
+MAIN_PROJECT_URL = "https://botw.link"
+SHOW_UNDOCUMENTED = True
+SEARCH_RESULT_ID_BYTES = 4
+SEARCH_FILE_OFFSET_BYTES = 4
+SEARCH_NAME_SIZE_BYTES = 2
+SEARCH_DOWNLOAD_BINARY = True
