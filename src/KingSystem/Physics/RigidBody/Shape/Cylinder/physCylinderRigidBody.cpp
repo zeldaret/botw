872d02a0372139f3af800c9bcb526284a34df5e3
Léo Lam <leo@leolam.fr>
2022-02-03 13:17:51 +0100

    ksys/phys: Add CylinderRigidBody

--- src/KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderRigidBody.cpp
+++ src/KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderRigidBody.cpp
@@ -1,1 +1,75 @@
 #include "KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderRigidBody.h"
+#include <Havok/Physics2012/Dynamics/Entity/hkpRigidBody.h>
+#include "KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderShape.h"
+#include "KingSystem/Physics/RigidBody/physRigidBodyFactory.h"
+#include "KingSystem/Utils/SafeDelete.h"
+
+namespace ksys::phys {
+
+CylinderRigidBody* CylinderRigidBody::make(RigidBodyInstanceParam* param, sead::Heap* heap) {
+    return RigidBodyFactory::createCylinder(param, heap);
+}
+
+CylinderRigidBody::CylinderRigidBody(hkpRigidBody* hk_body, CylinderShape* shape,
+                                     ContactLayerType layer_type, const sead::SafeString& name,
+                                     bool set_flag_10, sead::Heap* heap)
+    : RigidBodyFromShape(hk_body, layer_type, name, set_flag_10, heap), mShape(shape) {}
+
+CylinderRigidBody::~CylinderRigidBody() {
+    if (hasFlag(RigidBody::Flag::_10) && mShape)
+        util::safeDelete(mShape);
+}
+
+void CylinderRigidBody::setRadius(float radius) {
+    if (mShape->setRadius(radius))
+        updateShape();
+}
+
+void CylinderRigidBody::setVertices(const sead::Vector3f& va, const sead::Vector3f& vb) {
+    if (mShape->setVertices(va, vb))
+        updateShape();
+}
+
+float CylinderRigidBody::getRadius() const {
+    return mShape->getRadius();
+}
+
+void CylinderRigidBody::getVertices(sead::Vector3f* va, sead::Vector3f* vb) const {
+    mShape->getVertices(va, vb);
+}
+
+void CylinderRigidBody::transformVertices(sead::Vector3f* va, sead::Vector3f* vb) {
+    lock();
+    const hkTransform& transform = getHkBody()->getMotion()->getTransform();
+    unlock();
+    mShape->transformVertices(va, vb, transform);
+}
+
+void CylinderRigidBody::setMaterialMask(const MaterialMask& mask) {
+    mShape->setMaterialMask(mask);
+}
+
+const MaterialMask& CylinderRigidBody::getMaterialMask() const {
+    return mShape->getMaterialMask();
+}
+
+float CylinderRigidBody::getVolume() {
+    return mShape->getVolume();
+}
+
+Shape* CylinderRigidBody::getShape_() {
+    return mShape;
+}
+
+const Shape* CylinderRigidBody::getShape_() const {
+    return mShape;
+}
+
+u32 CylinderRigidBody::getCollisionMasks(RigidBody::CollisionMasks* masks) {
+    masks->ignored_layers = ~mContactMask.getDirect();
+    masks->collision_filter_info = getCollisionFilterInfo();
+    masks->material_mask = getMaterialMask().getRawData();
+    return 0;
+}
+
+}  // namespace ksys::phys
--- src/KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderRigidBody.h
+++ src/KingSystem/Physics/RigidBody/Shape/Cylinder/physCylinderRigidBody.h
@@ -4,9 +4,36 @@
 
 namespace ksys::phys {
 
-// TODO
+class CylinderShape;
+
 class CylinderRigidBody : public RigidBodyFromShape {
     SEAD_RTTI_OVERRIDE(CylinderRigidBody, RigidBodyFromShape)
+public:
+    static CylinderRigidBody* make(RigidBodyInstanceParam* param, sead::Heap* heap);
+
+    CylinderRigidBody(hkpRigidBody* hk_body, CylinderShape* shape, ContactLayerType layer_type,
+                      const sead::SafeString& name, bool set_flag_10, sead::Heap* heap);
+    ~CylinderRigidBody() override;
+
+    void setRadius(float radius);
+    void setVertices(const sead::Vector3f& va, const sead::Vector3f& vb);
+
+    float getRadius() const;
+    void getVertices(sead::Vector3f* va, sead::Vector3f* vb) const;
+    void transformVertices(sead::Vector3f* va, sead::Vector3f* vb);
+
+    void setMaterialMask(const MaterialMask& mask);
+    const MaterialMask& getMaterialMask() const;
+
+    float getVolume() override;
+
+protected:
+    Shape* getShape_() override;
+    const Shape* getShape_() const override;
+    u32 getCollisionMasks(CollisionMasks* masks) override;
+
+private:
+    CylinderShape* mShape;
 };
 
 }  // namespace ksys::phys
