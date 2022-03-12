4758d41d697e59d4869c23fcf1d50c4d488ca867
theo3 <arisstephenson2@gmail.com>
2022-03-11 23:55:24 -0800

    map debug stubs

--- src/KingSystem/Map/mapDebug.cpp
+++ src/KingSystem/Map/mapDebug.cpp
@@ -1,3 +1,23 @@
 #include "KingSystem/Map/mapDebug.h"
+#include "KingSystem/ActorSystem/actActor.h"
 
-namespace ksys::map {}  // namespace ksys::map
+namespace ksys::map {
+
+bool printDebugMsg(Object* object, const sead::SafeString& msg, const char* config_name) {
+    if (object == nullptr)
+        return false;
+
+    makeProjectMapMuuntPath(sead::FixedSafeString<0x200>(), object);
+    return true;
+}
+
+bool printDebugMsg(ksys::act::Actor* actor, const sead::SafeString& msg, const char* config_name) {
+    Object* o = actor->getMapObject();
+    if (o == nullptr)
+        return false;
+
+    makeProjectMapMuuntPath(sead::FixedSafeString<0x200>(), o);
+    return true;
+}
+
+}  // namespace ksys::map
--- src/KingSystem/Map/mapDebug.h
+++ src/KingSystem/Map/mapDebug.h
@@ -2,10 +2,16 @@
 
 #include <prim/seadSafeString.h>
 
+namespace ksys::act {
+class Actor;
+}  // namespace ksys::act
+
 namespace ksys::map {
 
 class Object;
 
+void* makeProjectMapMuuntPath(const sead::SafeString& path, Object* object);
 bool printDebugMsg(Object* object, const sead::SafeString& msg, const char* config_name);
+bool printDebugMsg(ksys::act::Actor* actor, const sead::SafeString& msg, const char* config_name);
 
 }  // namespace ksys::map
