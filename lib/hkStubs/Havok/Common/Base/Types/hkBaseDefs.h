288b3421a07fb3042664f4bb58f851c8a533b26a
Léo Lam <leo@leolam.fr>
2022-03-29 19:13:34 +0200

    Havok: Fix HK_FORCE_INLINE to actually force inlining
    ...using the always_inline attribute

--- lib/hkStubs/Havok/Common/Base/Types/hkBaseDefs.h
+++ lib/hkStubs/Havok/Common/Base/Types/hkBaseDefs.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define HK_FORCE_INLINE inline
+#define HK_FORCE_INLINE inline __attribute__((always_inline))
 #define HK_ALWAYS_INLINE HK_FORCE_INLINE
 #define HK_NEVER_INLINE __attribute__((noinline))
 
