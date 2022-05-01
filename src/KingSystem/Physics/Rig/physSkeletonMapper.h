d80ccb83714865bc182a305e773ab5df8be0980d
Léo Lam <leo@leolam.fr>
2022-05-01 22:40:51 +0200

    ksys/phys: Add SkeletonMapper

---
+++ src/KingSystem/Physics/Rig/physSkeletonMapper.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <hostio/seadHostIONode.h>
+#include "KingSystem/Physics/Rig/physBoneAccessor.h"
+#include "KingSystem/Physics/Rig/physModelBoneAccessor.h"
+
+class hkaSkeletonMapper;
+
+namespace ksys::phys {
+
+class SkeletonMapper : public sead::hostio::Node {
+public:
+    SkeletonMapper();
+    virtual ~SkeletonMapper();
+
+    bool init(hkaSkeletonMapper* skeleton_mapper, hkaSkeletonMapper* model_skeleton_mapper,
+              gsys::Model* model, sead::Heap* heap);
+
+    void mapPoseA();
+    void mapPoseB();
+
+private:
+    BoneAccessor mBoneAccessor;
+    ModelBoneAccessor mModelBoneAccessor;
+    hkaSkeletonMapper* mMapperA{};
+    hkaSkeletonMapper* mMapperB{};
+};
+
+}  // namespace ksys::phys
