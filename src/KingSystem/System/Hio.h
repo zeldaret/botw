ca9c5d60e94385f63694380313b63a9c447c26f0
Tonycons-dev <70544875+Tonycons-dev@users.noreply.github.com>
2022-06-25 12:57:25 -0400

    Update Hio.h

--- src/KingSystem/System/Hio.h
+++ src/KingSystem/System/Hio.h
@@ -9,10 +9,9 @@ namespace ksys {
 class Hio final : public sead::hostio::Node {
     SEAD_SINGLETON_DISPOSER(Hio)
 
-public:
     Hio() = default;
     ~Hio() = default;
 };
 KSYS_CHECK_SIZE_NX150(Hio, 0x28);
 
-}  //namespace ksys
+}  // namespace ksys
