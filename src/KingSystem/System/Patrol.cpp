fe98f6f0f3347ce4ba9ec2ea56c5a65b4a21e903
Tonycons-dev <70544875+Tonycons-dev@users.noreply.github.com>
2022-06-25 15:12:31 -0400

    Add ksys::Patrol & ksys::Terminal
    parent 43e001017fcb36ecffe4cad35ba0b73a2de29be9 author Tonycons-dev <70544875+Tonycons-dev@users.noreply.github.com> 1656184351 -0400 committer Tonycons-dev <70544875+Tonycons-dev@users.noreply.github.com> 1656432051 -0400
    
    Squash commits again (last one messed up)

--- src/KingSystem/System/Patrol.cpp
+++ src/KingSystem/System/Patrol.cpp
@@ -1,7 +1,20 @@
 #include "KingSystem/System/Patrol.h"
+#include <devenv/seadEnvUtil.h>
+#include "KingSystem/Framework/GameConfig.h"
 
 namespace ksys {
 
 SEAD_SINGLETON_DISPOSER_IMPL(Patrol)
+
+Patrol::Patrol() {
+    mField0 = GameConfig::getInstance()->_3dd;
+
+    mHasAutoPlay = GameConfig::getInstance()->mPatrolFeatures.include("オートプレイ");
+    mHasAutoGenPatroller = GameConfig::getInstance()->mPatrolFeatures.include("AutoGenPatroller");
+    mLoadStaticPhysUnstableMapUnit = false;
+
+    mList.initOffset(8);
+    sead::EnvUtil::getEnvironmentVariable(&mBuildURL, "BUILD_URL");
+}
 
 }  // namespace ksys
