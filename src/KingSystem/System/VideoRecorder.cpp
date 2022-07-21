2fbb9a4e5d1e03ff1bfc05185b0d2f165e23c405
ecumber <ecumber05@gmail.com>
2022-07-21 16:11:10 -0700

    Fixes

--- src/KingSystem/System/VideoRecorder.cpp
+++ src/KingSystem/System/VideoRecorder.cpp
@@ -1,16 +1,17 @@
 #include "KingSystem/System/VideoRecorder.h"
 
 namespace ksys {
+    
 SEAD_SINGLETON_DISPOSER_IMPL(VideoRecorder)
 
 void VideoRecorder::postCalc() {
-    if (isCaptureEnabled()) {
-        u32 framenum = frame_number;  // not sure why a new variable is created instead of using
-                                      // frameNumber directly, might be a mistake?
-        sead::FixedSafeString<0x100> output;
-        output.format("%s/%04d.tga", filename.cstr(), framenum);
-        agl::utl::ScreenShotMgr::instance()->reserveCaptureWithDebugHeap(1, 1, &output, 0);
-        frame_number++;
-    }
+    if (!isCaptureEnabled())
+        return;
+    u32 framenum = mFrameNumber; 
+    sead::FixedSafeString<0x100> output;
+    output.format("%s/%04d.tga", mFilename.cstr(), framenum);
+    agl::utl::ScreenShotMgr::instance()->reserveCaptureWithDebugHeap(true, agl::TextureDataSerializerTGA::TGAFormat::_1, &output, false);
+    mFrameNumber++;
 }
+
 }  // namespace ksys
--- src/KingSystem/System/VideoRecorder.h
+++ src/KingSystem/System/VideoRecorder.h
@@ -3,6 +3,7 @@
 #include <basis/seadNew.h>
 #include <heap/seadDisposer.h>
 #include <prim/seadSafeString.h>
+#include <agl/utl/aglScreenShotMgr.h>   
 #include "KingSystem/Utils/Types.h"
 
 namespace ksys {
@@ -14,30 +15,17 @@ class VideoRecorder {
 
 public:
     void postCalc();
-    bool isCaptureEnabled() const { return capture_enabled; }
-    int getFrameNumber() const { return frame_number; }
-    sead::FixedSafeString<0x100> getFileName() const { return filename; }
+    bool isCaptureEnabled() const { return mCaptureEnabled; }
+    u32 getFrameNumber() const { return mFrameNumber; }
+    const sead::SafeString& getFileName() const { return mFilename; }
 
 private:
-    bool capture_enabled = false;  // 0x28
-    bool _29 = true;       // 0x29, this gets set to true but isn't used elsewhere, no idea why
-    u32 frame_number = 0;  // 0x2C
-    sead::FixedSafeString<0x100> filename;  // 0x30
+    bool mCaptureEnabled = false;
+    bool _29 = true;
+    u32 mFrameNumber = 0;
+    sead::FixedSafeString<0x100> mFilename;
 };
 
 KSYS_CHECK_SIZE_NX150(VideoRecorder, 0x148);
-}  // namespace ksys
 
-namespace agl::utl {  // temp because this namespace doesn't exist in the sead decomp yet, remove
-                      // this once added
-
-class ScreenShotMgr {
-    SEAD_SINGLETON_DISPOSER(ScreenShotMgr)
-    ScreenShotMgr() = default;
-    virtual ~ScreenShotMgr();
-
-public:
-    u64 reserveCaptureWithDebugHeap(bool somebool, u64 someint, const sead::SafeString*,
-                                    bool secondbool);
-};
-}  // namespace agl::utl
+}
