83ccccc7cd0ed9f6a5e969165ba713d1bbf0195e
ecumber <ecumber05@gmail.com>
2022-11-02 20:25:46 -0700

    Update NXArgs.cpp

--- src/KingSystem/System/NXArgs.cpp
+++ src/KingSystem/System/NXArgs.cpp
@@ -7,7 +7,8 @@ SEAD_SINGLETON_DISPOSER_IMPL(nxargs)
 void nxargs::init(sead::Heap* heap) {
     sead::Heap* nxargsheap = sead::ExpHeap::create(
         0x13E8, "nxargsHeap", heap, 8, sead::ExpHeap::HeapDirection::cHeapDirection_Reverse, false);
-    ResLaunchParamData* reslaunchdata = reinterpret_cast<ResLaunchParamData*>(new (nxargsheap) char[0x1000]);
+    ResLaunchParamData* reslaunchdata =
+        reinterpret_cast<ResLaunchParamData*>(new (nxargsheap) char[0x1000]);
     size_t unknown;
 
     while (nn::oe::TryPopLaunchParameter(&unknown, reslaunchdata, 0x1000)) {
@@ -74,31 +75,6 @@ void nxargs::allocEntries(sead::Heap* heap, nxargs::ResLaunchParamData* data) {
             }
         }
     }
-}
-
-// TODO
-void nxargs::handleArgs() {
-    ksys::act::InfoData* aid;
-    sead::BufferedSafeString* mapType;
-    char value[19];
-    if (mHasHandledArgs  // |
-                         //! DebugBoardMgr::hasLoadingScreenStarted() | // 710089bb14
-                         //! Root4::checkFlag() | // 71008bce34
-                         //! ksys::evt::Manager::hasEvent() //7100db1364
-    )
-        return;
-    if (mType == ArgsType::None)
-        return;
-    *mapType = ksys::StageInfo::getCurrentMapType();
-    if (*mapType != "TitleMenu" | *mapType != "STAGESELECT" | *mapType != "")
-        return;
-    if (!(ksys::gdt::getFlag_IsGet_PlayerStole2(0) && ksys::StageInfo::sIsMainField))
-        return;
-    if (this->mHasHandledArgs)
-        return;
-    if (!mNumEntries)
-        return;
-    aid = ksys::act::InfoData::instance();
 }
 
 }  // namespace ksys
