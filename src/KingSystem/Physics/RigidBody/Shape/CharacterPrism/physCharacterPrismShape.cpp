2e3b2c717a053d8761fcc1165bb50e7ba7a82aed
Briggs Baltzell <BriggsBaltzellB3@gmail.com>
2022-11-10 17:58:12 -0600

    Implemented CharacterPrismShape::setMaterialMask() and ::getMaterialMask()

--- src/KingSystem/Physics/RigidBody/Shape/CharacterPrism/physCharacterPrismShape.cpp
+++ src/KingSystem/Physics/RigidBody/Shape/CharacterPrism/physCharacterPrismShape.cpp
@@ -1,1 +1,14 @@
 #include "KingSystem/Physics/RigidBody/Shape/CharacterPrism/physCharacterPrismShape.h"
+#include "KingSystem/Physics/RigidBody/Shape/Polytope/physPolytopeShape.h"
+
+namespace ksys::phys {
+
+void CharacterPrismShape::setMaterialMask(const MaterialMask& mask) {
+    mShape->setMaterialMask(mask);
+}
+
+const MaterialMask& CharacterPrismShape::getMaterialMask() const {
+    return mShape->getMaterialMask();
+}
+
+}  // namespace ksys::phys
