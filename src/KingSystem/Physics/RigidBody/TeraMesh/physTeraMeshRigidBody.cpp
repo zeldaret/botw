02c2fc10218fe1af450e4483fd52a12f9a079857
Briggs Baltzell <BriggsBaltzellB3@gmail.com>
2022-11-21 10:24:41 -0600

    Implemented TeraMeshRigidBody ctor

--- src/KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.cpp
+++ src/KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.cpp
@@ -1,1 +1,14 @@
 #include "KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.h"
+#include "KingSystem/Physics/RigidBody/physRigidBodyParam.h"
+
+namespace ksys::phys {
+
+TeraMeshRigidBody::TeraMeshRigidBody(hkpRigidBody* hk_body, sead::Heap* heap)
+    : RigidBody(RigidBody::Type::TeraMesh, ContactLayerType::Entity, hk_body, "TeraMesh", heap,
+                true) {
+    RigidBodyInstanceParam param;
+    param.motion_type = MotionType::Fixed;
+    initMotionAccessor(param, heap, false);
+}
+
+}  // namespace ksys::phys
