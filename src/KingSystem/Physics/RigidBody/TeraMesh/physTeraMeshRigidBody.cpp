f63f528254660da67f1e7e1ce2eaec2c83b23251
Briggs Baltzell <BriggsBaltzellB3@gmail.com>
2022-11-21 10:58:30 -0600

    Implemented TeraMeshRigidBody::getCollisionMasks

--- src/KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.cpp
+++ src/KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.cpp
@@ -1,5 +1,7 @@
 #include "KingSystem/Physics/RigidBody/TeraMesh/physTeraMeshRigidBody.h"
+#include <Havok/Physics2012/Dynamics/Entity/hkpRigidBody.h>
 #include "KingSystem/Physics/RigidBody/physRigidBodyParam.h"
+#include "KingSystem/Physics/StaticCompound/physStaticCompoundUtil.h"
 
 namespace ksys::phys {
 
@@ -9,6 +11,20 @@ TeraMeshRigidBody::TeraMeshRigidBody(hkpRigidBody* hk_body, sead::Heap* heap)
     RigidBodyInstanceParam param;
     param.motion_type = MotionType::Fixed;
     initMotionAccessor(param, heap, false);
+}
+
+u32 TeraMeshRigidBody::getCollisionMasks(RigidBody::CollisionMasks* masks, const u32* unk,
+                                         const sead::Vector3f& contact_point) {
+    const auto *collidable = getHkBody()->getCollidable();
+    masks->ignored_layers = ~mContactMask;
+    if (unk != nullptr) {
+        getCollisionFilterInfoFromCollidable(masks, &masks->collision_filter_info, *collidable,
+                                             unk);
+        return 0;
+    }
+    masks->material_mask = collidable->getShape()->getUserData();
+    masks->collision_filter_info = collidable->getCollisionFilterInfo();
+    return 0;
 }
 
 }  // namespace ksys::phys
