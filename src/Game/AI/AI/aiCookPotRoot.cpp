faba69593cf2f761a02304a1e3fbb2734905fe01
Briggs Baltzell <BriggsBaltzellB3@gmail.com>
2022-12-15 11:52:04 -0600

    Added fields and handleMessage to CookPotRoot

--- src/Game/AI/AI/aiCookPotRoot.cpp
+++ src/Game/AI/AI/aiCookPotRoot.cpp
@@ -1,4 +1,11 @@
 #include "Game/AI/AI/aiCookPotRoot.h"
+#include "Game/DLC/aocHardModeManager.h"
+#include "Game/UI/uiPauseMenuDataMgr.h"
+#include "KingSystem/ActorSystem/Attention/actAttention.h"
+#include "KingSystem/ActorSystem/actActor.h"
+#include "KingSystem/Utils/Thread/Message.h"
+#include "KingSystem/World/worldEnvMgr.h"
+#include "KingSystem/World/worldManager.h"
 
 namespace uking::ai {
 
@@ -21,6 +28,55 @@ void CookPotRoot::leave_() {
 void CookPotRoot::loadParams_() {
     getMapUnitParam(&mInitBurnState_m, "InitBurnState");
     getAITreeVariable(&mCurrentCookResultHolder_a, "CurrentCookResultHolder");
+}
+
+// NON_MATCHING
+bool CookPotRoot::handleMessage_(const ksys::Message& message) {
+    // 着火: "ignition" or "on fire"
+    if (message.getType().value == (u32)ksys::act::AttActionCode::Cook) {
+        if (isCurrentChild("着火")) {
+            // TODO: sub_7100EDC4B8
+            // TODO: GameSceneSubsys12 class
+            const int num_ingredients = 5;  // TODO
+            _48 = true;
+            CookingMgr* cooking_mgr = CookingMgr::instance();
+            cooking_mgr->resetArgCookData(mCookArg, mCookIngredients, num_ingredients,
+                                                     mCookItem);
+
+            CookingMgr::BoostArg boost_arg{false, true};
+            if (ksys::world::Manager::instance()->getEnvMgr()->isInBloodMoonTimeRange()) {
+                boost_arg.always_boost = true;
+            }
+            if (cooking_mgr->cook(mCookArg, mCookItem, boost_arg)) {
+                if (const auto* hard_mode_manager = aoc::HardModeManager::instance()) {
+                    if (hard_mode_manager->checkFlag(aoc::HardModeManager::Flag::EnableHardMode)) {
+                        if (hard_mode_manager->isHardModeChangeOn(
+                                aoc::HardModeManager::HardModeChange::NerfHpRestore)) {
+                            hard_mode_manager->nerfHpRestore(&mCookItem.life_recover);
+                        }
+                    }
+                }
+                if (true /* TODO: callCookingDemo */) {
+                    ui::PauseMenuDataMgr::instance()->removeGrabbedItems();
+                    cooking_mgr->setCookItem(mCookItem);
+                    mHasFinishedCookItem = true;
+                    return true;
+                }
+                _48 = false;
+                mHasFinishedCookItem = false;
+                // TODO
+            } else {
+                _48 = false;
+                mHasFinishedCookItem = false;
+                // TODO
+            }
+        }
+    }
+    if (message.getType().value != (u32)ksys::act::AttActionCode::KillTime) {
+        return false;
+    }
+    // TODO: callDemo007_1
+    return isCurrentChild("着火");
 }
 
 }  // namespace uking::ai
