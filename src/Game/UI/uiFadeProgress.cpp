b96b1baa7e080ece8fea2addd75664c21b052b25
oxixes <oxixes@protonmail.com>
2023-01-07 21:53:49 +0100

    Implemented FadeProgress

---
+++ src/Game/UI/uiFadeProgress.cpp
@@ -0,0 +1,50 @@
+#include "Game/UI/uiFadeProgress.h"
+#include "Game/UI/uiUtils.h"
+
+namespace uking::ui {
+
+SEAD_SINGLETON_DISPOSER_IMPL(FadeProgress)
+
+void FadeProgress::init() {
+    mActive = false;
+    mProgress = 0.0;
+    mGaugeValue = 0.0;
+    mSteps = 1.0;
+}
+
+void FadeProgress::reset() {
+    init();
+}
+
+void FadeProgress::updateFadeScreen() {
+    if (mActive) {
+        f32 progress = mProgress;
+        f32 difference = progress - mGaugeValue;
+
+        if (difference > 0.1f) mSteps = 1.1;
+        if (difference > 0.2f) mSteps = 1.2;
+        if (progress > 0.94f) mSteps = 10.0;
+
+        if (difference <= 0.0001f && difference >= -0.0001f) mSteps = 1.0;
+
+        f32 newProgress = mGaugeValue + (mProgressPerStep * mSteps);
+        if (newProgress < progress) progress = newProgress;
+        mGaugeValue = progress;
+        applyScreenFade(progress);
+    }
+}
+
+void FadeProgress::setProgress(f32 value) {
+    if (mProgress < value)
+        mProgress = value;
+}
+
+f32 FadeProgress::getGaugeValue() {
+    return mGaugeValue;
+}
+
+void FadeProgress::setIsActive() {
+    mActive = true;
+}
+
+} // namespace uking::ui
---
+++ src/Game/UI/uiFadeProgress.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <heap/seadDisposer.h>
+
+namespace uking::ui {
+
+class FadeProgress {
+    SEAD_SINGLETON_DISPOSER(FadeProgress)
+    FadeProgress() = default;
+    ~FadeProgress() = default;
+
+public:
+    void init();
+    void reset();
+    void updateFadeScreen();
+    void setProgress(float value);
+    float getGaugeValue();
+    void setIsActive();
+
+private:
+    bool mActive = false;
+    f32 mProgress = 0.0;
+    f32 mGaugeValue = 0.0;
+    f32 mProgressPerStep = 0.002;
+    f32 mSteps = 1.0;
+};
+
+} // namespace uking::ui
