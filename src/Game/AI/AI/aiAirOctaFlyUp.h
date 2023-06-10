8d099f9009d348044fa9fe91af40ad6ec3df6edc
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-06-10 13:49:40 -0400

    Add files via upload

--- src/Game/AI/AI/aiAirOctaFlyUp.h
+++ src/Game/AI/AI/aiAirOctaFlyUp.h
@@ -21,13 +21,12 @@ class AirOctaFlyUp : public ksys::act::ai::Ai {
 public:
     explicit AirOctaFlyUp(const InitArg& arg);
     ~AirOctaFlyUp() override;
-    
+
     bool handleMessage_(const ksys::Message& message) override;
     bool init_(sead::Heap* heap) override;
     void enter_(ksys::act::ai::InlineParamPack* params) override;
     void leave_() override;
     void loadParams_() override;
-    void getRuntimeTypeInfo();
 
 protected:
     // static_param at offset 0x38
