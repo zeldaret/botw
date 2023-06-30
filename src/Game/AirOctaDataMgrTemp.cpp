1773dcab555297221de2ec0e4e332c1bd1ae3cb2
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-06-29 20:51:19 -0400

    hopefully the table is correct now

--- src/Game/AirOctaDataMgrTemp.cpp
+++ src/Game/AirOctaDataMgrTemp.cpp
@@ -1,10 +1,9 @@
 #include "Game/AirOctaDataMgrTemp.h"
-
+#include "KingSystem/System/VFRValue.h"
 namespace uking {
 
 void AirOctaDataMgr::sub_71002fb17c(){
     float result = unk_f0 + unk_110 + unk_114 + unk_118 + unk_11c;
-    unk_fc = result;
-    return;
+    unk_fc.y = result;
 } 
 }
--- src/Game/AirOctaDataMgrTemp.h
+++ src/Game/AirOctaDataMgrTemp.h
@@ -1,20 +1,22 @@
 #pragma once
 #include "KingSystem/ActorSystem/actActor.h"
 namespace uking {
-class AirOctaDataMgr  {
-    SEAD_RTTI_BASE(AirOctaDataMgr)
+class AirOctaDataMgr {
+    public: static const sead::RuntimeTypeInfo::Interface* getRuntimeTypeInfoStatic() { static const sead::RuntimeTypeInfo::Root typeInfo; return &typeInfo; } static bool checkDerivedRuntimeTypeInfoStatic( const sead::RuntimeTypeInfo::Interface* typeInfo) { const sead::RuntimeTypeInfo::Interface* clsTypeInfo = AirOctaDataMgr::getRuntimeTypeInfoStatic(); return typeInfo == clsTypeInfo; } virtual bool checkDerivedRuntimeTypeInfo(const sead::RuntimeTypeInfo::Interface* typeInfo) const { return checkDerivedRuntimeTypeInfoStatic(typeInfo); } virtual const sead::RuntimeTypeInfo::Interface* getRuntimeTypeInfo() const { return getRuntimeTypeInfoStatic(); }
     //friend uking::ai::AI::AirOctaFlyUp;
 public:
     ksys::act::BaseProcLink& getProc() { return mBaseProcLink; }
     void sub_71002fb17c();
-    /* 0x08 */ char unk_00[0x10];
+    /* 0x08 */ ksys::act::BaseProcLink unk_00;
     /* 0x18 */ ksys::act::BaseProcLink mBaseProcLink;
-    char placeFiller[0xC8]; 
+    char placeFiller[36]; 
+    /*0x3f */ float unk_3f;
+    char placeFiller2[158]; 
     /*0xf0 */ float unk_f0;
-    char placeFiller2[0x8]; 
-    /*0xfc*/ float unk_fc;
-    char placeFiller3[0x10]; 
-    /*0x110*/ float unk_110;
+    char placeFiller3[4];
+    sead::Vector3f unk_fc;
+    char placeFiller4[12]; 
+    /*0x110 */ float unk_110;
     /*0x114 */ float unk_114;
     /*0x118 */ float unk_118;
     /*0x11c */ float unk_11c;
