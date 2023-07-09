5c976448d8c3790b48f56bcbf10080ecbc9056b4
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-07-09 19:32:29 -0400

    Add files via upload

--- src/Game/AI/AI/AirOcta/AirOctaDataMgr.cpp
+++ src/Game/AI/AI/AirOcta/AirOctaDataMgr.cpp
@@ -1,1 +1,8 @@
+#include "Game/AI/AI/AirOcta/AirOctaDataMgr.h"
+namespace uking {
 
+void AirOctaDataMgr::ChangeOctasYheightMaybe() {
+    float result = vec_EC.y + unk_110 + unk_114 + unk_118 + unk_11c;
+    vec_F8.y = result;
+}
+}  // namespace uking
