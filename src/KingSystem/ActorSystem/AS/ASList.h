c86a858548bfd39c108c8a364c673f91ceb5a0aa
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-07-09 19:34:49 -0400

    Add files via upload

--- src/KingSystem/ActorSystem/AS/ASList.h
+++ src/KingSystem/ActorSystem/AS/ASList.h
@@ -1,7 +1,10 @@
 #pragma once
+#include "KingSystem/ActorSystem/actActor.h"
 namespace ksys::as {
 class ASList{
 public:
-void sub_710115b070( f32 a2, f32 a3, const sead::SafeString& a4, int a5, int a6, bool a7);
+void DelayStateChangeMaybe( f32 a2, f32 a3, const sead::SafeString& a4, int a5, int a6, bool a7);
+bool GoLimpFromHeadShotMaybe(u32 a1, const sead::SafeString& SafeString, u32 a2) ;
 };
-}
+
+} //namespace ksys::as
