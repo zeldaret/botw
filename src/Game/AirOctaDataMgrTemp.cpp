38306f2b0f0159bd87f4bcc84f2c128c1617780e
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-07-09 19:26:33 -0400

    Delete AirOctaDataMgrTemp.cpp

--- src/Game/AirOctaDataMgrTemp.cpp
+++
@@ -1,8 +0,0 @@
-#include "Game/AirOctaDataMgrTemp.h"
-namespace uking {
-
-void AirOctaDataMgr::sub_71002fb17c() {
-    float result = unk_f0 + unk_110 + unk_114 + unk_118 + unk_11c;
-    unk_fc.y = result;
-}
-}  // namespace uking
