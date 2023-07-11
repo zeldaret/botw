668fd37f6167a9a74cca08f828e92c4be2791e3d
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-07-10 21:26:11 -0400

    made names more descriptive
    the names are probably less accurate now though, i just looked at what the function seemed to be using them for

--- src/KingSystem/ActorSystem/AS/ASList.h
+++ src/KingSystem/ActorSystem/AS/ASList.h
@@ -3,7 +3,7 @@
 namespace ksys::as {
 class ASList{
 public:
-void DelayStateChangeMaybe( f32 a2, f32 a3, const sead::SafeString& a4, int a5, int a6, bool a7);
+void StartAnimationMaybe( f32 a2, f32 a3, const sead::SafeString& animation_ASDefine, int buffer_size, int string_length, bool a7);
 bool GoLimpFromHeadShotMaybe(u32 a1, const sead::SafeString& SafeString, u32 a2) ;
 };
 
