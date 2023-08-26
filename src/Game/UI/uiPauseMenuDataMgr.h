d60bbc7bdc362f57ba0f016c385626251a932bb3
Kinak338 <77231406+Kinak338@users.noreply.github.com>
2023-08-26 09:32:35 +0200

    name change uiPauseMenuDataMgr.h

--- src/Game/UI/uiPauseMenuDataMgr.h
+++ src/Game/UI/uiPauseMenuDataMgr.h
@@ -50,7 +50,7 @@ static_assert(NumPouchItemsMax == 420, "NumPouchItemsMax must be 420 for now");
 constexpr int ItemStackSizeMax = 999;
 
 // TODO: figure out what this is
-constexpr int NumPouch50 = 50;
+constexpr int NumTabMax = 50;
 
 constexpr int NumGrabbableItems = 5;
 
@@ -174,7 +174,7 @@ public:
 
     PouchItemType getType() const { return mType; }
     bool isEquipped() const { return mEquipped; }
-    u8 get25() const { return _25; }
+    bool isInInventory() const { return mInInventory; }
     const sead::SafeString& getName() const { return mName; }
     ItemUse getItemUse() const { return mItemUse; }
 
@@ -236,7 +236,7 @@ private:
     ItemUse mItemUse = ItemUse::Invalid;
     s32 mValue = 0;
     bool mEquipped = false;
-    u8 _25 = 1;
+    u8 mInInventory = 1;
     sead::FixedSafeString<64> mName;
     Data mData{};
     sead::FixedObjArray<sead::FixedSafeString<64>, NumIngredientsMax> mIngredients;
@@ -479,12 +479,12 @@ private:
     mutable sead::CriticalSection mCritSection;
     Lists mItemLists;
     sead::SafeArray<PouchItem**, NumPouchCategories> mListHeads;
-    sead::SafeArray<PouchItem*, NumPouch50> mArray1;
-    sead::SafeArray<PouchItemType, NumPouch50> mArray2;
+    sead::SafeArray<PouchItem*, NumTabMax> mArrayTabs;
+    sead::SafeArray<PouchItemType, NumTabMax> mArrayTabsType;
     PouchItem* mLastAddedItem{};
-    s32 _44490 = -1;
-    s32 _44494 = -1;
-    s32 _44498{};
+    u32 mLastAddedItemTab = 0xFFFFFFFF;
+    s32 mLastAddedItemSlot = -1;
+    s32 mNumSlot = 0;
     sead::SafeArray<GrabbedItemInfo, NumGrabbableItems> mGrabbedItems;
     PouchItem* mItem_444f0{};
     s32 _444f8 = -1;
@@ -499,7 +499,7 @@ private:
     PouchItem* mGoronSoulItem{};
     PouchItem* mZoraSoulItem{};
     PouchItem* mGerudoSoulItem{};
-    bool _44538 = false;
+    bool mCanSeeHealthBar = false;
     PouchItem mNewlyAddedItem;
 
     /// Indicates if a temporary inventory ("pouch for quest") is being used.
