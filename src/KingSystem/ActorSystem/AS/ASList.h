e33e9b017058c8c16fe0419691392fca68cc2c69
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-09-11 19:19:54 -0500

    Add files via upload

--- src/KingSystem/ActorSystem/AS/ASList.h
+++ src/KingSystem/ActorSystem/AS/ASList.h
@@ -3,8 +3,10 @@
 namespace ksys::as {
 class ASList{
 public:
-void StartAnimationMaybe( f32 a2, f32 a3, const sead::SafeString& animation_ASDefine, int buffer_size, int string_length, bool a7);
-bool GoLimpFromHeadShotMaybe(u32 a1, const sead::SafeString& SafeString, u32 a2) ;
+void startAnimationMaybe( f32 a2, f32 a3, const sead::SafeString& animation, int a5, int a6, bool a7);
+bool goLimpFromHeadShotMaybe(u32 a1, const sead::SafeString& a2, u32 a3); // x_8
+s64 x_2(u32 a1, int a2, bool m, u32 a4);
+u8 sub_710115D3B8();
 };
 
 } //namespace ksys::as
