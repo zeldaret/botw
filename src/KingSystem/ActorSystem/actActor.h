e1561aa3cbdbf1b9e1193e1bf793e263775920a4
bomba1749 <58524643+bomba1749@users.noreply.github.com>
2023-10-18 19:36:18 -0400

    Add files via upload

--- src/KingSystem/ActorSystem/actActor.h
+++ src/KingSystem/ActorSystem/actActor.h
@@ -18,10 +18,14 @@
 #include "KingSystem/Map/mapMubinIter.h"
 #include "KingSystem/Utils/AtomicLongBitFlag.h"
 #include "KingSystem/Utils/Thread/ActorMessageTransceiver.h"
-#include "Game/Damage/dmgDamageManagerBase.h"
+
 namespace gsys {
 class Model;
 }  // namespace gsys
+
+namespace uking::dmg{
+class DamageManagerBase;
+} // namespace uking::dmg
 
 namespace ksys {
 
@@ -232,11 +236,11 @@ const MesTransceiverId* getMesTransceiverId() const { return mMsgTransceiver.get
     virtual void m90();
     virtual void m91();
     virtual void m92();
-    virtual void m93();
+    virtual void m93(int a1, float a2);
     virtual void m94();
     virtual void m95();
     virtual void m96();
-    virtual void getChemicalStuff();
+    virtual Chemical* getChemicalStuff();
     virtual void getWeapons();
     virtual void getArmors();
     virtual void m100();
