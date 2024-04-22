594fa8a03666153a70fc80207e17c5e2b0c41595
trixie <braylonmooney2009@gmail.com>
2024-04-22 18:25:35 -0400

    trying to fix things

--- src/Game/gamePlayReport.h
+++ src/Game/gamePlayReport.h
@@ -6,12 +6,14 @@
 namespace ksys::qst {
 struct Quest;
 }
+
 namespace uking {
 
 void reportKorok(const sead::Vector3f& position);
 void reportDungeon(const sead::SafeString& name, const sead::SafeString& event);
 void reportQuestStep(const ksys::qst::Quest* quest, int step_index);
 void reportGetItem(const sead::Vector3f& pos, const sead::SafeString* targetActorName);
+
 // TODO: More functions
 
 class PlayReport : public ksys::PlayReport {
